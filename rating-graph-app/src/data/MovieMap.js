4dfc0d743c9cbbadb8862555e19d92b45b8202e7
manuelfrazao002 <pv23952@alunos.estgv.ipv.pt>
2025-07-10 01:39:34 +0100

    test

--- rating-graph-app/src/data/MovieMap.js
+++ rating-graph-app/src/data/MovieMap.js
@@ -1,19 +1,19 @@
 // src/constants/movieMap.js
 
 export const movieMap = {
-  toe: [
-    //IMDB
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=1334662158&single=true&output=csv",
-    //RatingChart & VoteChart & EpisodesTable
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=190829179&single=true&output=csv",
+  // toe: [
+  //   //IMDB
+  //   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=1334662158&single=true&output=csv",
+  //   //RatingChart & VoteChart & EpisodesTable
+  //   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=190829179&single=true&output=csv",
 
-    //ratinggraph
-    //RatingPage
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=1942396010&single=true&output=csv",
+  //   //ratinggraph
+  //   //RatingPage
+  //   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=1942396010&single=true&output=csv",
 
-    //VotesOvertime
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=1140739200&single=true&output=csv",    
-  ],
+  //   //VotesOvertime
+  //   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=1140739200&single=true&output=csv",    
+  // ],
   spacemetro: [
     //IMDB
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdRmNXNL3JC2_iTsTMWU35Sv07KCiXDND9iUTc_732abN72By3ZrkxM8Tv4VPkAoJ_tROMU3W9RG72/pub?gid=1334662158&single=true&output=csv",
@@ -54,16 +54,6 @@ export const movieMap = {
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjwnfmK3Dt4xtzx16jYOW15yAEe4HnwtTXYJ7hysBxOs1d5OVlegaboxUxOVcxb7xK7dp__0YOWplB/pub?gid=1942396010&single=true&output=csv",
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjwnfmK3Dt4xtzx16jYOW15yAEe4HnwtTXYJ7hysBxOs1d5OVlegaboxUxOVcxb7xK7dp__0YOWplB/pub?gid=1140739200&single=true&output=csv",
   ],
-  test: [
-    //IMDBData
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmQcTYrFmi-w54BZV-E8uYpXtcEbeWqwJ8kTJntqRXzZWr9J4pKE_ojOO1GFoH2Yt63g3dgR6U0UOz/pub?gid=1334662158&single=true&output=csv",
-    //DataBase TVSHOW
-    "",
-    //RatingGraphData
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmQcTYrFmi-w54BZV-E8uYpXtcEbeWqwJ8kTJntqRXzZWr9J4pKE_ojOO1GFoH2Yt63g3dgR6U0UOz/pub?gid=1942396010&single=true&output=csv",
-    //Daily Count
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmQcTYrFmi-w54BZV-E8uYpXtcEbeWqwJ8kTJntqRXzZWr9J4pKE_ojOO1GFoH2Yt63g3dgR6U0UOz/pub?gid=1140739200&single=true&output=csv",
-  ]
 };
 
 export const movies = Object.keys(movieMap).map((id) => ({
