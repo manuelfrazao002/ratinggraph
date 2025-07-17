97b6e9d69befb318074c48086500c361e35b579c
manuelfrazao002 <pv23952@alunos.estgv.ipv.pt>
2025-07-17 12:02:48 +0100

    small changes

--- rating-graph-app/src/data/MovieMap.js
+++ rating-graph-app/src/data/MovieMap.js
@@ -15,16 +15,15 @@ export const movieMap = {
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJw0CO3rwdc7Zuc_x-Gn2mx_SU15aSJDVKqij3HPAdeSJKyOys69vM8nOYOY19rJy_pV_V_S6uFWc1/pub?gid=1140739200&single=true&output=csv",    
   ],
   spacemetro: [
-    //IMDB
+    //IMDBData
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdRmNXNL3JC2_iTsTMWU35Sv07KCiXDND9iUTc_732abN72By3ZrkxM8Tv4VPkAoJ_tROMU3W9RG72/pub?gid=1334662158&single=true&output=csv",
-    //RatingChart & VoteChart & EpisodesTable
+    //DataBase TVSHOW
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdRmNXNL3JC2_iTsTMWU35Sv07KCiXDND9iUTc_732abN72By3ZrkxM8Tv4VPkAoJ_tROMU3W9RG72/pub?gid=190829179&single=true&output=csv",
 
-    //Ratinggraph
-    //RatingPage
+    //RatingGraphData
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdRmNXNL3JC2_iTsTMWU35Sv07KCiXDND9iUTc_732abN72By3ZrkxM8Tv4VPkAoJ_tROMU3W9RG72/pub?gid=1942396010&single=true&output=csv",
 
-    //VotesOvertime
+    //Daily Count
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdRmNXNL3JC2_iTsTMWU35Sv07KCiXDND9iUTc_732abN72By3ZrkxM8Tv4VPkAoJ_tROMU3W9RG72/pub?gid=1140739200&single=true&output=csv",
 
   ],
