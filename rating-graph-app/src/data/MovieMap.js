d9fc4163fd656c91be2038c49e3e15e60477e5aa
manuelfrazao002 <pv23952@alunos.estgv.ipv.pt>
2025-07-17 12:01:54 +0100

    remoção de series

--- rating-graph-app/src/data/MovieMap.js
+++ rating-graph-app/src/data/MovieMap.js
@@ -28,32 +28,6 @@ export const movieMap = {
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdRmNXNL3JC2_iTsTMWU35Sv07KCiXDND9iUTc_732abN72By3ZrkxM8Tv4VPkAoJ_tROMU3W9RG72/pub?gid=1140739200&single=true&output=csv",
 
   ],
-  goodfriends: [
-    //IMDBData
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vScPJopX9eFoeD6oaTZbHCcgjMeTIXGB82u5cWwtvgFIj8PhntZOO0xkkY3BOKOpr-aR1EotqsktrQR/pub?gid=1334662158&single=true&output=csv",
-    //DataBase TVSHOW
-    "",
-    //RatingGraphData
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vScPJopX9eFoeD6oaTZbHCcgjMeTIXGB82u5cWwtvgFIj8PhntZOO0xkkY3BOKOpr-aR1EotqsktrQR/pub?gid=1942396010&single=true&output=csv",
-    //Daily Count
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vScPJopX9eFoeD6oaTZbHCcgjMeTIXGB82u5cWwtvgFIj8PhntZOO0xkkY3BOKOpr-aR1EotqsktrQR/pub?gid=1140739200&single=true&output=csv",
-  ],
-  different: [
-    //IMDBData
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTV5Irw2LUv8r3bB3mRCtnFVlbTuxnB0W1oDXlRBs4j-w5rJrrLb_FaZtKStPfuyMWOIhJXPoM9oRLe/pub?gid=1334662158&single=true&output=csv",
-    //DataBase TVSHOW
-    "",
-    //RatingGraphData
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTV5Irw2LUv8r3bB3mRCtnFVlbTuxnB0W1oDXlRBs4j-w5rJrrLb_FaZtKStPfuyMWOIhJXPoM9oRLe/pub?gid=1942396010&single=true&output=csv",
-    //Daily Count
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTV5Irw2LUv8r3bB3mRCtnFVlbTuxnB0W1oDXlRBs4j-w5rJrrLb_FaZtKStPfuyMWOIhJXPoM9oRLe/pub?gid=1140739200&single=true&output=csv",
-  ],
-  darkcases: [
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjwnfmK3Dt4xtzx16jYOW15yAEe4HnwtTXYJ7hysBxOs1d5OVlegaboxUxOVcxb7xK7dp__0YOWplB/pub?gid=1334662158&single=true&output=csv",
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjwnfmK3Dt4xtzx16jYOW15yAEe4HnwtTXYJ7hysBxOs1d5OVlegaboxUxOVcxb7xK7dp__0YOWplB/pub?gid=190829179&single=true&output=csv",
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjwnfmK3Dt4xtzx16jYOW15yAEe4HnwtTXYJ7hysBxOs1d5OVlegaboxUxOVcxb7xK7dp__0YOWplB/pub?gid=1942396010&single=true&output=csv",
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjwnfmK3Dt4xtzx16jYOW15yAEe4HnwtTXYJ7hysBxOs1d5OVlegaboxUxOVcxb7xK7dp__0YOWplB/pub?gid=1140739200&single=true&output=csv",
-  ],
 };
 
 export const animeMap = {
