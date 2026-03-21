97bafc5eabcb0dd0bada3d85979d8ade7d149943
ManuelFrazao <manuel15frazao@gmail.com>
2026-03-21 06:21:26 +0000

    small fix

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,15 +1,27 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: "5432",
-    dialect: "postgres",
-  },
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: process.env.DB_HOST,
+        port: "5432",
+        dialect: "postgres",
+      }
+    );
 
 module.exports = { sequelize };
